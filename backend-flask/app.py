95f187d47e40042f22b38c0b4d14845b319e6e2c
Mario Flory <marioflory@bluewin.ch>
2023-12-12 20:18:44 +0000

    included links to notifications page

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -4,7 +4,6 @@ from flask_cors import CORS, cross_origin
 import os
 
 from services.home_activities import *
-from services.notifications_activities import *
 from services.user_activities import *
 from services.create_activity import *
 from services.create_reply import *
@@ -13,6 +12,8 @@ from services.message_groups import *
 from services.messages import *
 from services.create_message import *
 from services.show_activity import *
+from services.notifications_activities import *
+
 
 app = Flask(__name__)
 frontend = os.getenv('FRONTEND_URL')
